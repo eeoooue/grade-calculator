cf4598d5034fb9a1f984a5b80b4711a8b74bfa6b
eeoooue <pdmoorhouse@gmail.com>
2023-01-24 15:33:48 +0000

    now displays a single module w/ dropdown

--- main.js
+++ main.js
@@ -1,28 +1,50 @@
 
-
 import { Module } from "./module.js"
 
-const courseContainer = document.querySelector(".course-container")
+const courseSelector = document.getElementById("course-selector")
+courseSelector.addEventListener("input", (e) => SwapToModule(e.target.value))
 
-const DSAModule = new Module("Algorithms and Data Structures")
-DSAModule.addComponent("Coursework", 60, 100)
-DSAModule.addComponent("Exam", 40, 50)
-courseContainer.appendChild(DSAModule.moduleContainer)
+SwapToModule("441105")
 
+function SwapToModule(module_id){    
 
-const PPF = new Module("Programming Portfolio")
-PPF.addComponent("Lab Book", 50, 50)
-PPF.addComponent("Practical Exam", 25, 7)
-PPF.addComponent("Capstone Project", 25, 50)
-courseContainer.appendChild(PPF.moduleContainer)
+    console.log("hello")
 
+    const courseContainer = document.querySelector(".course-container")
+    courseContainer.innerHTML = ""
 
-const CT = new Module("Computational Thinking")
-CT.addComponent("Assessment A", 15, 15)
-CT.addComponent("Assessment B", 20, 20)
-CT.addComponent("Assessment C", 15, 15)
-CT.addComponent("Assessment D", 20, 20)
-CT.addComponent("Assessment E", 20, 20)
-CT.addComponent("Assessment F", 10, 10)
-courseContainer.appendChild(CT.moduleContainer)
+    const module = CreateModule(module_id)
+
+    courseContainer.appendChild(module.moduleContainer)
+}
+
+function CreateModule(module_id){
+
+    switch(module_id){
+
+        case "441105":
+            const module441105 = new Module("Algorithms and Data Structures")
+            module441105.addComponent("Coursework", 60, 100)
+            module441105.addComponent("Exam", 40, 50)
+            return module441105
+
+        case "441101":
+            const module441101 = new Module("Programming Portfolio")
+            module441101.addComponent("Lab Book", 50, 50)
+            module441101.addComponent("Practical Exam", 25, 7)
+            module441101.addComponent("Capstone Project", 25, 50)
+            return module441101
+
+        case "441104":
+            const module441104 = new Module("Computational Thinking")
+            module441104.addComponent("Assessment A", 15, 15)
+            module441104.addComponent("Assessment B", 20, 20)
+            module441104.addComponent("Assessment C", 15, 15)
+            module441104.addComponent("Assessment D", 20, 20)
+            module441104.addComponent("Assessment E", 20, 20)
+            module441104.addComponent("Assessment F", 10, 10)
+            return module441104
+    }
+}
+
 
