003e81ae8a94609dde0fc298fe3d9b68366890fb
eeoooue <pdmoorhouse@gmail.com>
2026-06-07 17:13:16 +0100

    added an academic year selector (auto populates)

--- docs/main.js
+++ docs/main.js
@@ -4,7 +4,23 @@ import { UniversityModule } from "./lib/university_module.js"
 const courseSelector = document.getElementById("course-selector")
 courseSelector.addEventListener("input", (e) => SwapToModule(e.target.value))
 
+PopulateAcademicYearSelector()
 SwapToModule("441105")
+
+async function PopulateAcademicYearSelector() {
+
+    const response = await fetch("university_modules.json");
+    const modules = await response.json();
+
+    const years = [...new Set(Object.values(modules).map(m => m.academic_year))].sort();
+
+    const yearSelector = document.getElementById("year-selector")
+    yearSelector.innerHTML = years.map(y => `<option value="${y}">${y}</option>`).join("")
+}
+
+
+
+
 
 async function SwapToModule(module_id) {
 
